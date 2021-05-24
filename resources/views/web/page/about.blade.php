91a73bd3302f69a2c9f060ce1e90760100d77045
minhtam696 <tam.tang@maxgroup.vn>
2021-05-24 17:01:25 +0700

    about

--- resources/views/web/page/about.blade.php
+++ resources/views/web/page/about.blade.php
@@ -2,7 +2,312 @@
 @section('content')
 
 
-// Code here 
+<section id="home-banner" class="clearfix">
+	<div id="home-slider">
+		<div>
+			<div class="home-banner-item">
+				<img src="/assets/web/images/about.jpg" class="img-responsive">
+				<div class="home-banner-box">
+					<div class="container">
+						<p>
+							<b>
+								ORGANIC FRESH 
+								<br/>
+								HONEY ONLY
+							</b>
+						</p>
+						<ul class="list-inline">
+							<li class="list-inline-item">
+								<img src="/assets/web/images/icon-calendar.png" height="20"> 
+								May 10, 2021
+							</li>
+							<li class="list-inline-item">
+								<img src="/assets/web/images/icon-location.png" height="20"> 
+								Bảo Lộc, Lâm Đồng
+							</li>
+						</ul>
+						<p class="center-xs">
+							<a href="#" class="btn btn-style-1 font-italic">
+								Chi Tiết
+							</a>
+						</p>
+					</div>
+				</div>
+			</div>
+		</div> 
+	</div>
+</section>
+
+<section id="product-list" class="clearfix padding-50">
+	<img src="/assets/web/images/product-bee.png" class="hidden-xs pr-bee-left">
+	<img src="/assets/web/images/product-bee-right.png" class="hidden-xs pr-bee-right">
+	<div class="container">
+		<div class="row">
+			<div class="col-12 col-md-3">
+				<div class="product-list-left">
+
+					<div class="product-left-item">
+						<p class="product-cat-title">
+							<b>Category</b>
+						</p>
+						<ul class="list-unstyled product-cat-menu">
+							<li class="active">
+								<a href="#!">
+									honey bee
+								</a>
+
+								<span class="collapsed" data-toggle="collapse" data-target="#ul_1">
+									<i class="fa fa-chevron-down"></i>
+								</span>
+								<ul id="ul_1" class="list-unstyled collapse show">
+									<li class="active">
+										<a href="#!">
+											SP 1
+										</a>
+									</li>
+									<li>
+										<a href="#!">
+											SP 2
+										</a>
+									</li>
+									<li>
+										<a href="#!">
+											SP 3
+										</a>
+									</li>
+								</ul>
+
+							</li>
+							<li>
+								<a href="#!">
+									bee polen
+								</a>
+
+								<span class="collapsed" data-toggle="collapse" data-target="#ul_2">
+									<i class="fa fa-chevron-down"></i>
+								</span>
+								<ul id="ul_2" class="list-unstyled collapse">
+									<li>
+										<a href="#!">
+											SP 1
+										</a>
+									</li>
+									<li>
+										<a href="#!">
+											SP 2
+										</a>
+									</li>
+									<li>
+										<a href="#!">
+											SP 3
+										</a>
+									</li>
+								</ul>
+							</li>
+						</ul>
+					</div>
+
+
+					<div class="product-left-item">
+						<p class="product-cat-title">
+							<b>Weight</b>
+						</p>
+						<div class="product-filter-option">
+							<div class="form-check">
+								<label class="form-check-label" role="button">
+									<input type="radio" class="form-check-input" name="weight">
+									<span></span>
+									Option 1
+								</label>
+							</div>
+							<div class="form-check">
+								<label class="form-check-label" role="button">
+									<input type="radio" class="form-check-input" name="weight">
+									<span></span>
+									Option 2
+								</label>
+							</div>
+						</div>
+					</div>
+
+
+					<div class="product-left-item">
+						<p class="product-cat-title">
+							<b>Price</b>
+						</p>
+						<div class="product-filter-option">
+							<div class="form-check">
+								<label class="form-check-label" role="button">
+									<input type="radio" class="form-check-input" name="price">
+									<span></span>
+									Option 1
+								</label>
+							</div>
+							<div class="form-check">
+								<label class="form-check-label" role="button">
+									<input type="radio" class="form-check-input" name="price">
+									<span></span>
+									Option 2
+								</label>
+							</div>
+						</div>
+					</div>
+
+
+					<div class="product-left-item">
+						<p class="product-cat-title">
+							<b>Best sellers</b>
+						</p>
+						<ul class="list-unstyled">
+							<li class="product-seller">
+								<a href="#!">
+									<img src="/assets/web/images/product.jpg">
+									<div class="product-seller-text">
+										<p class="product-seller-name">
+											<strong>
+												Mật ong TASEBE 500G
+											</strong>
+										</p>
+										<hr/>
+										<p class="product-seller-price">
+											<strong class="c_d1480b">390.000</strong> vnđ
+										</p>
+									</div>
+								</a>
+							</li>
+
+							<li class="product-seller">
+								<a href="#!">
+									<img src="/assets/web/images/product.jpg">
+									<div class="product-seller-text">
+										<p class="product-seller-name">
+											<strong>
+												Mật ong TASEBE 500G
+											</strong>
+										</p>
+										<hr/>
+										<p class="product-seller-price">
+											<strong class="c_d1480b">390.000</strong> vnđ
+										</p>
+									</div>
+								</a>
+							</li>
+						</ul>
+					</div>
+
+
+
+				</div>
+			</div>
+			<div class="col-12 col-md-9">
+				<div class="row">
+					<div class="col-12 col-md-10 offset-md-1">
+						<div class="product-list-right">
+							<div class="row">
+								<div class="col-md-4">
+									<div class="product-item">
+
+										<span class="product-sale">
+											sale
+										</span>
+
+										<a href="product-detail.html">
+											<p class="product-img">
+												<img src="/assets/web/images/product.jpg">
+											</p>
+											<p class="product-name">
+												Mật ong
+												TASABE 500G
+											</p>
+											<hr>
+											<p class="product-price">
+												<span>390.000</span> vnđ
+											</p>
+										</a>
+									</div>
+								</div>
+
+								<div class="col-md-4">
+									<div class="product-item">
+
+										<span class="product-sale">
+											sale
+										</span>
+
+										<a href="product-detail.html">
+											<p class="product-img">
+												<img src="/assets/web/images/product.jpg">
+											</p>
+											<p class="product-name">
+												Mật ong
+												TASABE 500G
+											</p>
+											<hr>
+											<p class="product-price">
+												<span>390.000</span> vnđ
+											</p>
+										</a>
+									</div>
+								</div>
+
+								<div class="col-md-4">
+									<div class="product-item">
+
+										<span class="product-sale">
+											sale
+										</span>
+
+										<a href="product-detail.html">
+											<p class="product-img">
+												<img src="/assets/web/images/product.jpg">
+											</p>
+											<p class="product-name">
+												Mật ong
+												TASABE 500G
+											</p>
+											<hr>
+											<p class="product-price">
+												<span>390.000</span> vnđ
+											</p>
+										</a>
+									</div>
+								</div>
+
+								<div class="clearfix"></div>
+
+								<div class="col-md-4">
+									<div class="product-item">
+
+										<a href="product-detail.html">
+											<p class="product-img">
+												<img src="/assets/web/images/product.jpg">
+											</p>
+											<p class="product-name">
+												Mật ong
+												TASABE 500G
+											</p>
+											<hr>
+											<p class="product-price">
+												<span>390.000</span> vnđ
+											</p>
+										</a>
+									</div>
+								</div>
+
+							</div>
+							<div class="text-center">
+								<a href="#!" class="btn btn-style-1">
+									<i>Xem thêm</i>
+								</a>
+							</div>
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
 
 
 @endsection
@@ -10,5 +315,5 @@
 
 @endsection
 @section('js')
-    
+
 @endsection
--- resources/views/web/page/detailProducts.blade.php
+++ resources/views/web/page/detailProducts.blade.php
@@ -1,12 +1,363 @@
 @extends('web.master')
 @section('content')
 
-// Code here 
+<section id="product-banner" class="clearfix">
+	<img src="/assets/web/images/product-banner.jpg">
+	<div class="container">
+		<div class="product-cat-name">
+			<b>
+				sản phẩm
+			</b>
+			<ul class="list-inline text-center">
+				<li class="list-inline-item">
+					<a href="">
+						Home
+					</a>
+				</li>
+				/
+				<li>
+					<a href="">
+						Products
+					</a>
+				</li>
+				/
+				<li>
+					<a href="">
+						Mật Ong Tasabee
+					</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</section>
+
+<section id="product-detail" class="clearfix padding-50">
+
+	<div class="container">
+		<div class="product-detail-box">
+			<div class="row">
+				<div class="col-md-8">
+					<div class="product-detail-left">
+						<div class="row">
+							<div class="col-3 no-padding-right">
+								<div class="product-vertical-slider">
+									<div>
+										<div class="product-vertical-slide">
+											<img src="/assets/web/images/product.jpg" class="img-responsive">
+										</div>
+									</div>
+									<div>
+										<div class="product-vertical-slide">
+											<img src="/assets/web/images/product.jpg" class="img-responsive">
+										</div>
+									</div>
+									<div>
+										<div class="product-vertical-slide">
+											<img src="/assets/web/images/product.jpg" class="img-responsive">
+										</div>
+									</div>
+									<div>
+										<div class="product-vertical-slide">
+											<img src="/assets/web/images/product.jpg" class="img-responsive">
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="col-9">
+								<div class="product-main-slider">
+									<div>
+										<div class="product-main-slide">
+											<img src="/assets/web/images/product.jpg" class="img-responsive">
+										</div>
+									</div>
+									<div>
+										<div class="product-main-slide">
+											<img src="/assets/web/images/product.jpg" class="img-responsive">
+										</div>
+									</div>
+									<div>
+										<div class="product-main-slide">
+											<img src="/assets/web/images/product.jpg" class="img-responsive">
+										</div>
+									</div>
+									<div>
+										<div class="product-main-slide">
+											<img src="/assets/web/images/product.jpg" class="img-responsive">
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="product-detail-right">
+						<h1 class="product-detail-title text-uppercase">
+							<strong>
+								Mật ong TASABE
+							</strong>
+						</h1>
+
+						<p class="product-detail-price">
+							<strong class="c_d1480b">390.000</strong> vnđ
+						</p>
+
+						<div class="product-detail-excerpt">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. 
+						</div>
+
+						<hr class="product-detail-hr" />
+
+						<p class="no-margin"><strong>Size:</strong></p>
+
+						<ul class="product-detail-size list-inline">
+							<li class="active">
+								<a href="#!">
+									10 gm
+								</a>
+							</li>
+							<li>
+								<a href="#!">
+									50 gm
+								</a>
+							</li>
+							<li>
+								<a href="#!">
+									100 gm
+								</a>
+							</li>
+							<li>
+								<a href="#!">
+									200 gm
+								</a>
+							</li>
+							<li>
+								<a href="#!">
+									250 gm
+								</a>
+							</li>
+							<li>
+								<a href="#!">
+									300 gm
+								</a>
+							</li>
+						</ul>
+
+						<div class="product-detail-btn">
+							<div class="row align-items-center">
+								<div class="col-6">
+									<a href="#!" class="btn btn-style-1 btn-100">
+										<i>
+											Liên hệ
+										</i>
+									</a>
+								</div>
+								<div class="col-6">
+									<div class="d-flex align-items-center justify-content-around">
+										<small class="d-flex align-items-center">CHIA SẺ &nbsp<img src="/assets/web/images/icon-share.png" width="15"></small> 
+										<a href="">
+											<img src="/assets/web/images/icon-fb.png" width="24">
+										</a>
+										<a href="">
+											<img src="/assets/web/images/icon-twiter.png" width="24">
+										</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+		<div class="product-detail-tab">
+			<ul class="nav nav-tabs" id="product-tab-nav" role="tablist">
+				<li class="nav-item">
+					<a class="nav-link active" data-toggle="tab" href="#tab-1">
+						Description
+					</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link " data-toggle="tab" href="#tab-2">
+						Write review
+					</a>
+				</li>
+			</ul>
+			<div class="tab-content" id="tab-content-product">
+				<div class="tab-pane fade show active" id="tab-1">
+					<div class="text-justify">
+						<p>
+							MẬT ONG TASABE
+						</p>
+						<p>
+							390.000 vnđ
+						</p>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.
+						</p>
+						<p class="text-center">
+							<img src="/assets/web/images/product.jpg">
+						</p>
+					</div>
+				</div>
+				<div class="tab-pane fade " id="tab-2">
+					<p>
+						<strong>Customer Reviews</strong>
+					</p>
+					<p>
+						<strong>SANDRA RUIZ</strong>
+						<br/>
+						May 10, 2021
+					</p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. 
+					<hr class="product-detail-hr">
+					<p>
+						<strong>
+							Leave a comment:
+						</strong>
+						<br/>
+						Your email address will not be published. Required fields are marked *
+					</p>
+					<div class="row">
+						<div class="col-12 col-md-6">
+							<div class="frm-cmt">
+								<div class="form-group">
+									<textarea class="form-control" rows="5" placeholder="Your Message"></textarea>
+								</div>
+								<div class="form-group">
+									<input type="text" class="form-control" placeholder="Your Name">
+								</div>
+								<div class="form-group">
+									<input type="email" class="form-control" placeholder="Your Email">
+								</div>
+								<div class="center-xs">
+									<button class="btn btn-style-1">Submit</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+	</div>
+</section>
+
+<section id="orther-product" class="padding-50 clearfix">
+	<div class="container">
+		<h2 class="home-title text-uppercase text-center">
+			<b>
+				Sản Phẩm <span class="c_ff7200">Khác</span>
+			</b>
+		</h2>
+		<p class="text-center">
+			quis nostrud quam est, qui dolorem ipsum 
+			<br>
+			quis nostrud quam est, qui dolorem ipsum quia dolor sit amet, consecquaerat 
+		</p>
+		<div class="row">
+			<div class="col-12 col-md-10 offset-md-1">
+				<div class="padding-25">
+					<div class="home-silder-product">
+						<div>
+							<div class="slide-product">
+								<a href="#!">
+									<p class="slide-product-img">
+										<img src="/assets/web/images/product.jpg">
+									</p>
+									<p class="slide-product-name">
+										Mật ong
+										TASABE 500G
+									</p>
+									<hr/>
+									<p class="slide-product-price">
+										<span>390.000</span> vnđ
+									</p>
+								</a>
+							</div>
+						</div>
+						<div>
+							<div class="slide-product">
+								<a href="#!">
+									<p class="slide-product-img">
+										<img src="/assets/web/images/product.jpg">
+									</p>
+									<p class="slide-product-name">
+										Mật ong
+										TASABE 500G
+									</p>
+									<hr/>
+									<p class="slide-product-price">
+										<span>390.000</span> vnđ
+									</p>
+								</a>
+							</div>
+						</div>
+						<div>
+							<div class="slide-product">
+								<a href="#!">
+									<p class="slide-product-img">
+										<img src="/assets/web/images/product.jpg">
+									</p>
+									<p class="slide-product-name">
+										Mật ong
+										TASABE 500G
+									</p>
+									<hr/>
+									<p class="slide-product-price">
+										<span>390.000</span> vnđ
+									</p>
+								</a>
+							</div>
+						</div>
+						<div>
+							<div class="slide-product">
+								<a href="#!">
+									<p class="slide-product-img">
+										<img src="/assets/web/images/product.jpg">
+									</p>
+									<p class="slide-product-name">
+										Mật ong
+										TASABE 500G
+									</p>
+									<hr/>
+									<p class="slide-product-price">
+										<span>390.000</span> vnđ
+									</p>
+								</a>
+							</div>
+						</div>
+						<div>
+							<div class="slide-product">
+								<a href="#!">
+									<p class="slide-product-img">
+										<img src="/assets/web/images/product.jpg">
+									</p>
+									<p class="slide-product-name">
+										Mật ong
+										TASABE 500G
+									</p>
+									<hr/>
+									<p class="slide-product-price">
+										<span>390.000</span> vnđ
+									</p>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
 
 @endsection
 @section('css')
 
 @endsection
 @section('js')
-    
+
 @endsection

8aeef2e6e233e50f4520e5eb3ca87b86bab03f7a
minhtam696 <tam.tang@maxgroup.vn>
2021-05-25 16:39:19 +0700

    blog

--- resources/views/web/master.blade.php
+++ resources/views/web/master.blade.php
@@ -10,13 +10,13 @@
 	<meta name="msapplication-TileImage" content="/assets/web/images/icon-bee.png" />
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 	<meta name="author" content="Tasabee">
-    <meta name="description" content="@yield('meta_description')">
-    <meta name="keywords" content="@yield('meta_keywords')">
-    <link rel="canonical" href="{!! url()->current() !!}">
-    <meta property="og:title" content="@yield('meta_title')">
-    <meta property="og:description" content="@yield('meta_description')">
-    <meta property="og:image" content="{{ url('/')}}@yield('image')"/>
-    <meta property="og:url" content="{!! url()->current() !!}">
+	<meta name="description" content="@yield('meta_description')">
+	<meta name="keywords" content="@yield('meta_keywords')">
+	<link rel="canonical" href="{!! url()->current() !!}">
+	<meta property="og:title" content="@yield('meta_title')">
+	<meta property="og:description" content="@yield('meta_description')">
+	<meta property="og:image" content="{{ url('/')}}@yield('image')"/>
+	<meta property="og:url" content="{!! url()->current() !!}">
 	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
 	<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
 	<link href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css" rel="stylesheet" type="text/css" />
@@ -136,7 +136,104 @@
 
 
 	@yield('content')
-
+	<!--Footer-->
+	<section id="footer-contact" class="clearfix padding-25">
+		<div class="container">
+			<p class="text-center">
+				<img src="/assets/web/images/icon-bee.png" width="45">
+			</p>
+			<h2 class="home-title text-uppercase text-center c_ff7200">
+				<b>
+					<span class="c_222">Liên Hệ Với</span> Chúng Tôi
+				</b>
+			</h2>
+		</div>
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-12 col-md-4 no-padding">
+					<p class="footer-contact-img">
+						<img src="/assets/web/images/b-pr.jpg">
+					</p>
+				</div>
+				<div class="col-12 col-md-6">
+					<div class="row">
+						<div class="col-12 col-md-6">
+							<div class="footer-contact-left">
+								<div class="contact-left-item">
+									<span>
+										Showrom 1:
+									</span>
+									<br/>
+									Đường 1/5, Tp.Bảo Lộc, Lâm Đồng
+									<br/>
+									<b>Hotline:</b> 0981.477.642
+								</div>
+								<div class="contact-left-item">
+									<span>
+										Showrom 2:
+									</span>
+									<br/>
+									30B Trần Bình Trọng, Tp. Nha Trang
+									<br/>
+									<b>Hotline:</b> 0905.391.333
+								</div>
+								<div class="contact-left-item">
+									<span>
+										Showrom 3:
+									</span>
+									<br/>
+									153G Lũy Bán Bích, Tân Thới Hòa,
+									Tân Phú, Tp.HCM
+									<br/>
+									<b>Hotline:</b> 0917.778.96
+								</div>
+							</div>
+						</div>
+						<div class="col-12 col-md-6">
+							<div class="footer-contact-right">
+								<p class="text-justify">
+									Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.
+								</p>
+								<p>
+									<b>
+										CÔNG TY TNHH XUẤT NHẬP KHẨU TASABEE
+									</b>
+								</p>
+								<ul class="list-unstyled contact-address">
+									<li>
+										<span>A</span>	Thôn 13, Xã DamBri, Bảo Lộc, Lâm Đồng
+									</li>
+									<li>
+										<span>T</span>	0917778968
+									</li>
+									<li>
+										<span>E</span>	info@tasabe.com
+									</li>
+								</ul>
+								<ul class="list-inline center-xs contact-links">
+									<li class="list-inline-item">
+										<a href="#!">
+											f
+										</a>
+									</li>
+									<li class="list-inline-item">
+										<a href="#!">
+											t
+										</a>
+									</li>
+									<li class="list-inline-item">
+										<a href="#!">
+											g
+										</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
 	<footer id="footer" class="clearfix">
 		<div class="container">
 			<div class="row align-items-center">
@@ -149,6 +246,8 @@
 			</div>
 		</div>
 	</footer>
+
+	@include('web.inc.popup')
 
 	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js"></script>

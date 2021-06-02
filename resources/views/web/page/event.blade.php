06051f40d0d4a4601dbfe041d2c7e9f82149fb6d
minhtam696 <tam.tang@maxgroup.vn>
2021-06-02 18:50:57 +0700

    events page

--- resources/views/web/page/event.blade.php
+++ resources/views/web/page/event.blade.php
@@ -2,91 +2,342 @@
 @section('meta_title', $page && $page->meta_title ? $page->meta_title : $settings->meta_title)
 @section('meta_description', $page && $page->meta_description ? $page->meta_description : $settings->meta_description)
 @section('image', $page && $page->image ? $page->image : $settings->image)
-@section('content') 
+@section('content')
 
-<section class="bg-top clearfix">
-	<img src="/assets/web/images/bg-blog.jpg">
+<section id="home-banner" class="clearfix">
+	<div id="home-slider">
+
+		<div>
+			<div class="home-banner-item">
+				<img src="/uploads/images/banner-1.jpg" class="img-responsive" alt="">
+				<div class="home-banner-box">
+					<div class="container">
+						<p>
+							<b>{{ trans('web.menu.event') }}</b>
+						</p>
+						<ul class="list-inline">
+							<li class="list-inline-item">
+								<img src="/assets/web/images/icon-calendar.png" height="20"> May 10, 2021
+							</li>
+							<li class="list-inline-item">
+								<img src="/assets/web/images/icon-location.png" height="20"> Bảo Lộc
+							</li>
+						</ul>
+
+						<p class="center-xs">
+							<a href="" class="btn btn-style-1 font-italic" title="">
+								Xem thêm
+							</a>
+						</p>
+
+					</div>
+				</div>
+			</div>
+		</div> 
+
+	</div>
+</section>
+
+<section id="event" class="padding-25">
 	<div class="container">
-		<div class="top-cat-name">
+		<p class="text-center">
+			<img src="/assets/web/images/icon-bee.png" width="45">
+		</p>
+		<h2 class="home-title text-uppercase text-center c_ff7200">
 			<b>
-				{{ trans('web.menu.event') }}
+				<span class="c_222">event -</span> sự kiện nổi bật
 			</b>
+		</h2>
+		<div class="list-events">
+			<div class="event-1">
+				<div class="row">
+
+					<div class="col-12 col-md-6">
+
+						<div class="event-item">
+							<div class="row">
+								<div class="col-12 col-md-5">
+									<p class="event-img">
+										<img src="/assets/web/images/news.png">
+									</p>
+								</div>
+								<div class="col-12 col-md-7">
+									<hr>
+									<h3>
+										<a href="#!" class="c_222">
+											<strong>
+												Neque Porro Quisquam
+											</strong>
+										</a>
+									</h3>
+									<ul class="list-inline">
+										<li class="list-inline-item">
+											<img src="/assets/web/images/event-icon-1.png" height="20"> 2021-05-05
+										</li>
+										<li class="list-inline-item">
+											<img src="/assets/web/images/event-icon-2.png" height="20"> Bao Loc lam dong aa
+										</li>
+									</ul>
+									<p class="text-justify">
+										Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+									</p>
+									<div class="text-right center-xs">
+										<div class="d-flex align-items-center justify-flex-end">
+											<small class="d-flex align-items-center">CHIA SẺ &nbsp;<img src="/assets/web/images/icon-share.png" width="15"></small> 
+											<span style="margin: 0 5px;">&nbsp;</span>
+											<a href="">
+												<img src="/assets/web/images/icon-fb.png" width="24">
+											</a>
+											<span style="margin: 0 5px;">&nbsp;</span>
+											<a href="">
+												<img src="/assets/web/images/icon-twiter.png" width="24">
+											</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+					</div>
+
+
+					<div class="col-12 col-md-6">
+						<div class="event-video">
+							<a data-fancybox href="#!">
+								<img src="/assets/web/images/news.png" class="img-responsive">
+							</a>
+						</div>
+					</div>
+
+
+					<div class="col-12 col-md-6">
+
+						<div class="event-item">
+							<div class="row">
+								<div class="col-12 col-md-5">
+									<p class="event-img">
+										<img src="/assets/web/images/news.png">
+									</p>
+								</div>
+								<div class="col-12 col-md-7">
+									<hr>
+									<h3>
+										<a href="#!" class="c_222">
+											<strong>
+												Neque Porro Quisquam
+											</strong>
+										</a>
+									</h3>
+									<ul class="list-inline">
+										<li class="list-inline-item">
+											<img src="/assets/web/images/event-icon-1.png" height="20"> 2021-05-05
+										</li>
+										<li class="list-inline-item">
+											<img src="/assets/web/images/event-icon-2.png" height="20"> Bao Loc lam dong aa
+										</li>
+									</ul>
+									<p class="text-justify">
+										Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+									</p>
+									<div class="text-right center-xs">
+										<div class="d-flex align-items-center justify-flex-end">
+											<small class="d-flex align-items-center">CHIA SẺ &nbsp;<img src="/assets/web/images/icon-share.png" width="15"></small> 
+											<span style="margin: 0 5px;">&nbsp;</span>
+											<a href="">
+												<img src="/assets/web/images/icon-fb.png" width="24">
+											</a>
+											<span style="margin: 0 5px;">&nbsp;</span>
+											<a href="">
+												<img src="/assets/web/images/icon-twiter.png" width="24">
+											</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+					</div>
+
+
+
+					<div class="col-12 col-md-6">
+
+						<div class="event-item">
+							<div class="row">
+								<div class="col-12 col-md-5">
+									<p class="event-img">
+										<img src="/assets/web/images/news.png">
+									</p>
+								</div>
+								<div class="col-12 col-md-7">
+									<hr>
+									<h3>
+										<a href="#!" class="c_222">
+											<strong>
+												Neque Porro Quisquam
+											</strong>
+										</a>
+									</h3>
+									<ul class="list-inline">
+										<li class="list-inline-item">
+											<img src="/assets/web/images/event-icon-1.png" height="20"> 2021-05-05
+										</li>
+										<li class="list-inline-item">
+											<img src="/assets/web/images/event-icon-2.png" height="20"> Bao Loc lam dong aa
+										</li>
+									</ul>
+									<p class="text-justify">
+										Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+									</p>
+									<div class="text-right center-xs">
+										<div class="d-flex align-items-center justify-flex-end">
+											<small class="d-flex align-items-center">CHIA SẺ &nbsp;<img src="/assets/web/images/icon-share.png" width="15"></small> 
+											<span style="margin: 0 5px;">&nbsp;</span>
+											<a href="">
+												<img src="/assets/web/images/icon-fb.png" width="24">
+											</a>
+											<span style="margin: 0 5px;">&nbsp;</span>
+											<a href="">
+												<img src="/assets/web/images/icon-twiter.png" width="24">
+											</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+					</div>
+
+
+
+
+
+				</div>
+			</div>
 		</div>
 	</div>
 </section>
 
-<section class="blog-box padding-50">
-	<img src="/assets/web/images/blog-bee-left.png" class="hidden-xs blog-bee-left">
-	<img src="/assets/web/images/blog-bee-right.png" class="hidden-xs blog-bee-right">
-
-	<div class="container">
-		<div class="blog-search-frm">
-			<form method="get">
-				<input type="text" class="form-control" placeholder="{{ trans('web.field.search') }}" name="keyword" required value="{{ $keyword }}">
-				<button>
-					<img src="/assets/web/images/icon-search.png">
-				</button>
-			</form>
+<section class="about-banner-box">
+	<img src="/assets/web/images/about-banner-2.jpg">
+	<div class="about-banner-text">
+		<div>
+			<h3 class="text-uppercase">
+				<strong>
+					memories from event 2020
+				</strong>
+			</h3>
+			<p>
+				Check our amazing last year moments
+			</p>
 		</div>
-		<div class="row">
-			@if ($posts)
-				@foreach ($posts as $post)
-					<div class="col-12 col-md-4">
-						<div class="blog-item">
-							<a href="{!! route('detailBlog', $post->slug) !!}" title="{{ $post->title }}">
-								<p class="blog-img">
-									<img src="{!! $post->image !!}" class="img-responsive" alt="{!! $post->title !!}">
-								</p>
-								<div class="blog-name">
-									<b>
-										{!! $post->title !!}
-									</b>
-								</div>
-								<p class="blog-date">
-									{!! $post->created_at !!}
-								</p>
-								<p class="blog-description">
-									{!! $post->expert !!}
-								</p>
-							</a>
-							<div class="text-center">
-								<a href="{!! route('detailBlog', $post->slug) !!}" class="btn btn-style-1" title="{{ $post->title }}">
-									Chi tiết
-								</a>
-							</div>
+	</div>
+</section>
+
+<section class="container-fluid">
+	<div class="row">
+		<div class="col-6 col-md-4 no-padding">
+			<div class="gallery-item">
+				<a href="#!">
+					<img src="/assets/web/images/gallery.png" class="img-responsive">
+					<div class="gallery-box">
+						<div class="text-center">
+							<p>
+								EVENT SỰ KIỆN 1
+							</p>
+							<hr>
+							<img src="/assets/web/images/icon-plug.png" width="25">
 						</div>
-					</div> 	
-				@endforeach
-			@endif
+					</div>
+				</a>
+			</div>
 		</div>
-		@if($posts->items() && !empty($posts->items()))
-			<nav class="text-center pagination-new">
-				<ul class="pagination justify-content-center">
-					@if ($posts->currentPage() > 1)
-						<li class="page-item">
-							<a class="page-link" href="{!! url()->current() !!}?page={!! $posts->currentPage() - 1 !!}" aria-label="Previous">
-								<span aria-hidden="true">&laquo;</span>
-								<span class="sr-only">Previous</span>
-							</a>
-						</li>
-					@endif
-					@for( $i=1;$i <= $posts->lastPage(); $i++)
-						<li class="page-item {{ $i == $posts->currentPage() ? 'active' : '' }}">
-							<a class="page-link" href="{!! url()->current() !!}?page={!! $i !!}">{!! $i !!}</a>
-						</li> 
-					@endfor
-					@if ($posts->currentPage() < $posts->lastPage())
-						<li class="page-item">
-							<a class="page-link" href="{!! url()->current() !!}?page={!! $posts->currentPage() + 1 !!}" aria-label="Next">
-								<span aria-hidden="true">&raquo;</span>
-								<span class="sr-only">Next</span>
-							</a>
-						</li>
-					@endif
-				</ul>
-			</nav>
-		@endif
+		<div class="col-6 col-md-4 no-padding">
+			<div class="gallery-item">
+				<a href="#!">
+					<img src="/assets/web/images/gallery.png" class="img-responsive">
+					<div class="gallery-box">
+						<div class="text-center">
+							<p>
+								EVENT SỰ KIỆN 1
+							</p>
+							<hr>
+							<img src="/assets/web/images/icon-plug.png" width="25">
+						</div>
+					</div>
+				</a>
+			</div>
+		</div>
+		<div class="col-6 col-md-4 no-padding">
+			<div class="gallery-item">
+				<a href="#!">
+					<img src="/assets/web/images/gallery.png" class="img-responsive">
+					<div class="gallery-box">
+						<div class="text-center">
+							<p>
+								EVENT SỰ KIỆN 1
+							</p>
+							<hr>
+							<img src="/assets/web/images/icon-plug.png" width="25">
+						</div>
+					</div>
+				</a>
+			</div>
+		</div>
 
+
+		<div class="clearfix"></div>
+		
+		<div class="col-6 col-md-4 no-padding">
+			<div class="gallery-item">
+				<a href="#!">
+					<img src="/assets/web/images/gallery.png" class="img-responsive">
+					<div class="gallery-box">
+						<div class="text-center">
+							<p>
+								EVENT SỰ KIỆN 1
+							</p>
+							<hr>
+							<img src="/assets/web/images/icon-plug.png" width="25">
+						</div>
+					</div>
+				</a>
+			</div>
+		</div>
+		<div class="col-6 col-md-4 no-padding">
+			<div class="gallery-item">
+				<a href="#!">
+					<img src="/assets/web/images/gallery.png" class="img-responsive">
+					<div class="gallery-box">
+						<div class="text-center">
+							<p>
+								EVENT SỰ KIỆN 1
+							</p>
+							<hr>
+							<img src="/assets/web/images/icon-plug.png" width="25">
+						</div>
+					</div>
+				</a>
+			</div>
+		</div>
+		<div class="col-6 col-md-4 no-padding">
+			<div class="gallery-item">
+				<a href="#!">
+					<img src="/assets/web/images/gallery.png" class="img-responsive">
+					<div class="gallery-box">
+						<div class="text-center">
+							<p>
+								EVENT SỰ KIỆN 1
+							</p>
+							<hr>
+							<img src="/assets/web/images/icon-plug.png" width="25">
+						</div>
+					</div>
+				</a>
+			</div>
+		</div>
+
+		<div class="clearfix"></div>
 	</div>
 </section>
 
